2690133b65e2632d5e9897a249bb569d17effbb4
Walkimist <paulovitortheobald@gmail.com>
2026-02-04 10:11:10 -0300

    Refactor function and variable names to follow PEP8 naming conventions

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ WIDTH = 980
 HEIGHT = 720
 
 
-def makeAnimationFramesDict(prefix):
+def make_animation_frames_dict(prefix):
     return {
         "idleLeft": [f"{prefix}_idle_1l", f"{prefix}_idle_2l"],
         "idleRight": [f"{prefix}_idle_1r", f"{prefix}_idle_2r"],
@@ -20,18 +20,18 @@ def makeAnimationFramesDict(prefix):
     }
 
 
-PLAYER_ANIMATION_FRAMES = makeAnimationFramesDict("player")
-CATERPILLAR_ANIMATION_FRAMES = makeAnimationFramesDict("enemy1")
-WASP_ANIMATION_FRAMES = makeAnimationFramesDict("enemy2")
-CAT_ANIMATION_FRAMES = makeAnimationFramesDict("enemy3")
+PLAYER_ANIMATION_FRAMES = make_animation_frames_dict("player")
+CATERPILLAR_ANIMATION_FRAMES = make_animation_frames_dict("enemy1")
+WASP_ANIMATION_FRAMES = make_animation_frames_dict("enemy2")
+CAT_ANIMATION_FRAMES = make_animation_frames_dict("enemy3")
 
 
 WAVE_PROPERTIES = {
-    1: {"amount": 15, "upgrade": 5, "spawn delay": 2},
-    2: {"amount": 20, "upgrade": 50, "spawn delay": 2},
-    3: {"amount": 30, "upgrade": 10, "spawn delay": 1},
-    4: {"amount": 30, "upgrade": 50, "spawn delay": 2},
-    5: {"amount": 50, "upgrade": 50, "spawn delay": 1},
+    1: {"amount": 15, "upgrade": 5, "delay": 2},
+    2: {"amount": 20, "upgrade": 50, "delay": 2},
+    3: {"amount": 30, "upgrade": 10, "delay": 1},
+    4: {"amount": 30, "upgrade": 50, "delay": 2},
+    5: {"amount": 50, "upgrade": 50, "delay": 1},
 }
 
 HITBOXES = {"small": 12, "medium": 15, "big": 20}
@@ -74,84 +74,84 @@ class Entity(Actor):
         position,
         movespeed=1,
         frames={},
-        maxHealth=1,
+        max_health=1,
         hitbox=0,
-        hurtSound=None,
+        hurt_sound=None,
     ):
         super().__init__(image, position)
         self.movespeed = movespeed
         self.frames = frames
-        self.maxHealth = maxHealth
-        self.currentHealth = maxHealth
-        self.isHurt = False
-        self.isMoving = False
+        self.max_health = max_health
+        self.current_health = max_health
+        self.is_hurt = False
+        self.is_moving = False
         self.direction = [0, 0]
-        self.facingDirection = 1
-        self.hitboxSize = hitbox
-        self.hurtSound = hurtSound
+        self.facing_direction = 1
+        self.hitbox_size = hitbox
+        self.hurt_sound = hurt_sound
 
-        self.currentFrame = 0
-        self.frameId = 0
+        self.current_frame = 0
+        self.frame_id = 0
 
-    def updateAnimation(self):
-        animationState = self.getCurrentAnimationState()
+    def update_animation(self):
+        animation_state = self.get_current_animation_state()
 
         if self.direction[0] < 0:
-            self.facingDirection = 0
+            self.facing_direction = 0
         elif self.direction[0] > 0:
-            self.facingDirection = 1
+            self.facing_direction = 1
 
-        self.currentFrame += 1
+        self.current_frame += 1
         # we update the animation every couple of frames to control its speed
-        if self.currentFrame % ANIMATION_INTERVAL == 0:
-            self.frameId += 1
-        if self.frameId >= len(self.frames[animationState]):
-            self.frameId = 0
-        self.image = self.frames[animationState][self.frameId]
+        if self.current_frame % ANIMATION_INTERVAL == 0:
+            self.frame_id += 1
+        if self.frame_id >= len(self.frames[animation_state]):
+            self.frame_id = 0
+        self.image = self.frames[animation_state][self.frame_id]
 
-    def getCurrentAnimationState(self):
-        if self.currentHealth <= 0:
-            if self.facingDirection:
+    def get_current_animation_state(self):
+        if self.current_health <= 0:
+            if self.facing_direction:
                 return "deadRight"
             return "deadLeft"
-        if self.isHurt:
-            if self.facingDirection:
+        if self.is_hurt:
+            if self.facing_direction:
                 return "hurtRight"
             return "hurtLeft"
-        if self.isMoving:
-            if self.facingDirection:
+        if self.is_moving:
+            if self.facing_direction:
                 return "walkRight"
             return "walkLeft"
-        if self.facingDirection:
+        if self.facing_direction:
             return "idleRight"
         return "idleLeft"
 
-    def changeCurrentHealth(self, amount):
-        self.currentHealth += amount
+    def change_current_health(self, amount):
+        self.current_health += amount
         if amount < 0:
-            self.isHurt = True
-            getattr(sounds, self.hurtSound).play()
-            clock.schedule(self.setCharacterNormal, 0.2)
+            self.is_hurt = True
+            getattr(sounds, self.hurt_sound).play()
+            clock.schedule(self.set_character_normal, 0.2)
 
-    def setCharacterNormal(self):
-        self.isHurt = False
+    def set_character_normal(self):
+        self.is_hurt = False
 
     def move(self):
         # get the directional magnitude to normalize the speed in diagonal directions
         magnitude = math.hypot(self.direction[0], self.direction[1])
 
         if magnitude == 0:
-            self.isMoving = False
+            self.is_moving = False
             return
 
-        self.isMoving = True
+        self.is_moving = True
 
         self.x += (self.direction[0] / magnitude) * self.movespeed
         self.y += (self.direction[1] / magnitude) * self.movespeed
 
-    def checkIfLeftScreen(self, list):
+    def check_if_left_screen(self, entity_list):
         if self.left > WIDTH or self.right < 0 or self.bottom < 0 or self.top > HEIGHT:
-            list.remove(self)
+            entity_list.remove(self)
 
 
 class Projectile(Entity):
@@ -160,57 +160,57 @@ class Projectile(Entity):
         self.direction = direction
         self.damage = damage
 
-    def playSound(self):
-        soundId = random.randint(1, 3)
-        getattr(sounds, f"shoot{soundId}").play()
+    def play_sound(self):
+        sound_id = random.randint(1, 3)
+        getattr(sounds, f"shoot{sound_id}").play()
 
 
 class Player(Entity):
     def __init__(
-        self, image, position, movespeed, frames, maxHealth, hitbox, hurtSound
+        self, image, position, movespeed, frames, max_health, hitbox, hurt_sound
     ):
         super().__init__(
-            image, position, movespeed, frames, maxHealth, hitbox, hurtSound
+            image, position, movespeed, frames, max_health, hitbox, hurt_sound
         )
-        self.isInvulnerable = False
-        self.invulnerabilityTime = 0.5
-        self.shootingCooldown = 0.8
-        self.canShoot = True
+        self.is_invulnerable = False
+        self.invulnerability_time = 0.5
+        self.shooting_cooldown = 0.8
+        self.can_shoot = True
 
-    def updateShooting(self):
+    def update_shooting(self):
         global projectiles
-        if self.canShoot:
+        if self.can_shoot:
             projectile = Projectile(
                 "player_projectile",
                 self.pos,
                 SPEED["projectile"],
                 HITBOXES["small"],
-                self.getMouseDirection(),
+                self.get_mouse_direction(),
                 DAMAGE["weak"],
             )
             projectiles.append(projectile)
-            projectile.playSound()
-            self.canShoot = False
-            clock.schedule(self.reload, self.shootingCooldown)
+            projectile.play_sound()
+            self.can_shoot = False
+            clock.schedule(self.reload, self.shooting_cooldown)
 
     def reload(self):
-        self.canShoot = True
+        self.can_shoot = True
 
-    def getMouseDirection(self):
-        radiansToMouse = math.atan2(mousePos[1] - self.y, mousePos[0] - self.x)
+    def get_mouse_direction(self):
+        radians_to_mouse = math.atan2(mouse_pos[1] - self.y, mouse_pos[0] - self.x)
 
-        xDirection = math.cos(radiansToMouse)
-        yDirection = math.sin(radiansToMouse)
-        return [xDirection, yDirection]
+        x_direction = math.cos(radians_to_mouse)
+        y_direction = math.sin(radians_to_mouse)
+        return [x_direction, y_direction]
 
-    def activateInvulnerability(self):
-        self.isInvulnerable = True
-        clock.schedule(self.deactivateInvulnerability, self.invulnerabilityTime)
+    def activate_invulnerability(self):
+        self.is_invulnerable = True
+        clock.schedule(self.deactivate_invulnerability, self.invulnerability_time)
 
-    def deactivateInvulnerability(self):
-        self.isInvulnerable = False
+    def deactivate_invulnerability(self):
+        self.is_invulnerable = False
 
-    def updatePressedDirection(self):
+    def update_pressed_direction(self):
         self.direction = [0, 0]
 
         if keyboard[keys.A] or keyboard[keys.LEFT]:
@@ -226,165 +226,171 @@ class Player(Entity):
 
 class Enemy(Entity):
     def __init__(
-        self, image, position, movespeed, frames, maxHealth, hitbox, damage, hurtSound
+        self, image, position, movespeed, frames, max_health, hitbox, damage, hurt_sound
     ):
         super().__init__(
-            image, position, movespeed, frames, maxHealth, hitbox, hurtSound
+            image, position, movespeed, frames, max_health, hitbox, hurt_sound
         )
         self.damage = damage
-        self.removalTime = 1
+        self.removal_time = 1
 
-    def checkPlayerCollision(self, player):
-        distanceToPlayer = self.distance_to(player)
+    def check_player_collision(self, player):
+        distance_to_player = self.distance_to(player)
         if (
-            distanceToPlayer < self.hitboxSize + player.hitboxSize
-            and player.isInvulnerable == False
+            distance_to_player < self.hitbox_size + player.hitbox_size
+            and player.is_invulnerable == False
         ):
-            player.changeCurrentHealth(-self.damage)
-            player.activateInvulnerability()
+            player.change_current_health(-self.damage)
+            player.activate_invulnerability()
 
-    def checkProjectileCollisions(self, projectiles):
+    def check_projectile_collisions(self, projectiles):
         for projectile in projectiles:
             if projectile:
-                distanceToProjectile = self.distance_to(projectile)
-                if distanceToProjectile < self.hitboxSize + projectile.hitboxSize:
-                    self.changeCurrentHealth(-projectile.damage)
-                    if self.currentHealth <= 0:
-                        clock.schedule(self.removeSelf, self.removalTime)
+                distance_to_projectile = self.distance_to(projectile)
+                if distance_to_projectile < self.hitbox_size + projectile.hitbox_size:
+                    self.change_current_health(-projectile.damage)
+                    if self.current_health <= 0:
+                        clock.schedule(self.remove_self, self.removal_time)
                     projectiles.remove(projectile)
 
-    def updatePlayerDirection(self, player):
-        if player.currentHealth > 0:
-            angleTowardsPlayer = self.angle_to(player)
+    def update_player_direction(self, player):
+        if player.current_health > 0:
+            angle_towards_player = self.angle_to(player)
         else:
             self.movespeed = SPEED["slow"]
-            angleTowardsPlayer = self.angle_to(player) + 180
-        radiansTowardsPlayer = math.radians(angleTowardsPlayer)
+            angle_towards_player = self.angle_to(player) + 180
+        radians_towards_player = math.radians(angle_towards_player)
 
-        xDirection = math.cos(radiansTowardsPlayer)
-        yDirection = -math.sin(radiansTowardsPlayer)
+        x_direction = math.cos(radians_towards_player)
+        y_direction = -math.sin(radians_towards_player)
 
-        self.direction = [xDirection, yDirection]
+        self.direction = [x_direction, y_direction]
 
-    def removeSelf(self):
+    def remove_self(self):
         global enemies
         enemies.remove(self)
 
 
 class WaveManager:
     def __init__(self):
-        self.currentWave = 1
+        self.current_wave = 1
         self.intermission = False
-        self.spawnsRemaining = WAVE_PROPERTIES[self.currentWave]["amount"]
+        self.spawns_remaining = WAVE_PROPERTIES[self.current_wave]["amount"]
 
-        self.waveText = Actor("wave_text", ((WIDTH / 2), -50))
-        self.waveNumber = Actor(f"{self.currentWave}", ((WIDTH / 2 + 34), -50))
+        self.wave_text = Actor("wave_text", ((WIDTH / 2), -50))
+        self.wave_number = Actor(f"{self.current_wave}", ((WIDTH / 2 + 34), -50))
 
-    def startWave(self):
-        self.spawnEnemies()
-        animate(self.waveText, "out_elastic", pos=((WIDTH / 2), 30))
-        animate(self.waveNumber, "out_elastic", pos=((WIDTH / 2 + 34), 30))
-        clock.schedule(self.retractWaveText, 3)
+    def start_wave(self):
+        self.spawn_enemies()
+        animate(self.wave_text, "out_elastic", pos=((WIDTH / 2), 30))
+        animate(self.wave_number, "out_elastic", pos=((WIDTH / 2 + 34), 30))
+        clock.schedule(self.retract_wave_text, 3)
 
-    def retractWaveText(self):
-        animate(self.waveText, "accelerate", pos=((WIDTH / 2), -50))
-        animate(self.waveNumber, "accelerate", pos=((WIDTH / 2 + 34), -50))
+    def retract_wave_text(self):
+        animate(self.wave_text, "accelerate", pos=((WIDTH / 2), -50))
+        animate(self.wave_number, "accelerate", pos=((WIDTH / 2 + 34), -50))
 
-    def spawnEnemies(self):
+    def spawn_enemies(self):
         # we choose an edge of the screen to spread out enemy spawns
-        spawnRegion = random.randint(0, 3)
+        spawn_region = random.randint(0, 3)
         OFFSET = 50
-        match (spawnRegion):
+        match (spawn_region):
             case 0:  # top
-                posX = random.randint(0, WIDTH)
-                posY = -OFFSET
+                pos_x = random.randint(0, WIDTH)
+                pos_y = -OFFSET
             case 1:  # left
-                posX = -OFFSET
-                posY = random.randint(0, HEIGHT)
+                pos_x = -OFFSET
+                pos_y = random.randint(0, HEIGHT)
             case 2:  # right
-                posX = WIDTH + OFFSET
-                posY = random.randint(0, HEIGHT)
+                pos_x = WIDTH + OFFSET
+                pos_y = random.randint(0, HEIGHT)
             case 3:  # bottom
-                posX = random.randint(0, WIDTH)
-                posY = HEIGHT + OFFSET
-        enemyType = self.getEnemyType()
-        enemyData = ENEMY_TYPES[enemyType]
+                pos_x = random.randint(0, WIDTH)
+                pos_y = HEIGHT + OFFSET
+        enemy_type = self.get_enemy_type()
+        enemy_data = ENEMY_TYPES[enemy_type]
         enemy = Enemy(
-            data["image"],
-            (posX, posY),
-            data["speed"],
-            data["frames"],
-            data["health"],
-            data["hitbox"],
-            data["damage"],
+            enemy_data["image"],
+            (pos_x, pos_y),
+            enemy_data["speed"],
+            enemy_data["frames"],
+            enemy_data["health"],
+            enemy_data["hitbox"],
+            enemy_data["damage"],
             "enemy_hurt",
         )
         global enemies
         enemies.append(enemy)
-        self.spawnsRemaining -= 1
-        if self.spawnsRemaining > 0 and self.intermission == False:
+        self.spawns_remaining -= 1
+        if self.spawns_remaining > 0 and self.intermission == False:
             clock.schedule(
-                self.spawnEnemies, WAVE_PROPERTIES[self.currentWave]["spawn delay"]
+                self.spawn_enemies, WAVE_PROPERTIES[self.current_wave]["delay"]
             )
         else:
-            self.waitForWaveEnd()
+            self.wait_for_wave_end()
 
-    def waitForWaveEnd(self):
-        if self.currentWave == 5:
+    def wait_for_wave_end(self):
+        if self.current_wave == 5:
             # end game
             return
         if len(enemies) > 0:
-            clock.schedule(self.waitForWaveEnd, 1)
+            clock.schedule(self.wait_for_wave_end, 1)
             return
-        self.endWave()
+        self.end_wave()
 
-    def endWave(self):
+    def end_wave(self):
         getattr(sounds, "wave_pass").play()
         self.intermission = True
-        self.currentWave += 1
-        self.spawnsRemaining = WAVE_PROPERTIES[self.currentWave]["amount"]
+        self.current_wave += 1
+        self.spawns_remaining = WAVE_PROPERTIES[self.current_wave]["amount"]
 
-    def getEnemyType(self):
+    def get_enemy_type(self):
         # we will roll two 'dice', each with a chance to further upgrade the enemy for difficulty and randomness
         roll = [random.randint(1, 100), random.randint(1, 100)]
-        if roll[0] <= WAVE_PROPERTIES[self.currentWave]["upgrade chance"]:
-            if roll[1] <= WAVE_PROPERTIES[self.currentWave]["upgrade chance"]:
+        if roll[0] <= WAVE_PROPERTIES[self.current_wave]["upgrade"]:
+            if roll[1] <= WAVE_PROPERTIES[self.current_wave]["upgrade"]:
                 return "cat"
             return "wasp"
         return "caterpillar"
 
 
-def getBackgroundImage():
+def get_background_image():
     roll = random.randint(1, 4)
     return f"background_{roll}"
 
 
-player = Player(
-    "player_idle_1r",
-    (WIDTH / 2, HEIGHT / 2),
-    SPEED["very fast"],
-    PLAYER_ANIMATION_FRAMES,
-    HEALTH["very tough"],
-    HITBOXES["big"],
-    "player_hurt",
-)
-
+mouse_pos = [0, 0]
+background = get_background_image()
+player = None
+wave_manager = None
 projectiles = []
 enemies = []
 
-waveManager = WaveManager()
-waveManager.startWave()
 
-music.play("kim-lightyear-leave-the-world-tonight-chiptune-edit-loop-132102")
-music.set_volume(0.02)
+def start_game():
+    global player
+    global wave_manager
 
-mousePos = [0, 0]
-background = getBackgroundImage()
+    player = Player(
+        "player_idle_1r",
+        (WIDTH / 2, HEIGHT / 2),
+        SPEED["very fast"],
+        PLAYER_ANIMATION_FRAMES,
+        HEALTH["very tough"],
+        HITBOXES["big"],
+        "player_hurt",
+    )
+
+    wave_manager = WaveManager()
+    wave_manager.start_wave()
+
+    music.play("kim-lightyear-leave-the-world-tonight-chiptune-edit-loop-132102")
+    music.set_volume(0.02)
 
 
 def on_mouse_move(pos, rel, buttons):
-    global mousePos
-    mousePos = pos
+    global mouse_pos
+    mouse_pos = pos
 
 
 def draw():
@@ -395,24 +401,24 @@ def draw():
     for projectile in projectiles:
         projectile.draw()
     player.draw()
-    waveManager.waveText.draw()
-    waveManager.waveNumber.draw()
+    wave_manager.wave_text.draw()
+    wave_manager.wave_number.draw()
 
 
 def update():
-    player.updateAnimation()
-    if player.currentHealth > 0:
-        player.updatePressedDirection()
+    player.update_animation()
+    if player.current_health > 0:
+        player.update_pressed_direction()
         player.move()
-        player.updateShooting()
+        player.update_shooting()
     for enemy in enemies:
-        # enemy.checkIfLeftScreen(enemies)
-        if enemy.currentHealth > 0:
-            enemy.updatePlayerDirection(player)
+        # enemy.check_if_left_screen(enemies)
+        if enemy.current_health > 0:
+            enemy.update_player_direction(player)
             enemy.move()
-            enemy.checkProjectileCollisions(projectiles)
-            enemy.checkPlayerCollision(player)
-        enemy.updateAnimation()
+            enemy.check_projectile_collisions(projectiles)
+            enemy.check_player_collision(player)
+        enemy.update_animation()
     for projectile in projectiles:
-        projectile.checkIfLeftScreen(projectiles)
+        projectile.check_if_left_screen(projectiles)
         projectile.move()
